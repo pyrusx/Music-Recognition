8838da8fdf0c5c3940afce9e7824cff38fe0cd0e
abhiraj-saxena <a.s.abhiraj.saxena@gmail.com>
2019-10-15 20:51:55 -0400

    added number of octaves, note to freq functions

--- tuner.py
+++ tuner.py
@@ -7,3 +7,15 @@ NOTES = 'C C# D D# E F F# G G# A A# B'.split()
 MIN_FREQ = 27.5 #A0
 
 MAX_FREQ = 4186.0 #C8
+
+REF_FREQ = 440 #A4
+
+def number_octaves(freq1, freq2):
+    return np.log2(freq2/freq1)
+
+def note_to_frequency(note):
+    return (2^(note/12)) * REF_FREQ
+
+def 
+# All semitones have ratio of 2^(1/12)
+print(number_octaves(23, 4))
